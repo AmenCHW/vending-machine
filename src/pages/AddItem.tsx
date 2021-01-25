c7a86b69b3f271e04e130aaea645e4797ba9d90c
Krausso <apvbd17@gmail.com>
2021-01-25 20:51:44 +0200

    master; AddItem page update && dispatch is actually working

--- src/pages/AddItem.tsx
+++ src/pages/AddItem.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { addItemAction } from "../store/actions/addItem";
+import { itemI } from "../interfaces/item.interface";
 
 const AddItem: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    const item: itemI = {
+      id: "test",
+      title: "Cola",
+      image_url: "https://bit.ly/2YcWUxc",
+      quantity: 6
+    }
+    dispatch(addItemAction(item))
+    console.log("clicked")
+  }
+
   return (
-    <h1>Add Item page</h1>
+    <button onClick={handleClick}>
+      Add item
+    </button>
   );
 }
 
