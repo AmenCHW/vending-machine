47e4beba996a4aac4d8aca494c39d5b75d9586e6
Krausso <apvbd17@gmail.com>
2021-01-27 18:52:23 +0200

    dev; rootReducer updated with addCash action && initialState updated

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,14 +1,19 @@
 import { createReducer } from "@reduxjs/toolkit";
 import { initialStateT } from "../types/initialState.type";
 import { addItemAction } from "./actions/addItem";
+import { addCashAction } from "./actions/addCash";
 
 const initialState: initialStateT = {
-  items: []
+  items: [],
+  cash: 0
 }
 
 const rootReducer = createReducer(initialState, {
   [addItemAction.type]: (state, action) => {
     return {...state, items: [...state.items, action.payload]}
+  },
+  [addCashAction.type]: (state, action) => {
+    return {...state, cash: state.cash+action.payload}
   }
 })
 
--- src/types/initialState.type.ts
+++ src/types/initialState.type.ts
@@ -1,5 +1,6 @@
 import { itemT } from "./item.type";
 
 export type initialStateT = {
-  items: Array<itemT>
+  items: Array<itemT>,
+  cash: number
 }
