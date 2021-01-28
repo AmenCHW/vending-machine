5546548bc4eba6aff80b17173b80c2b6e27c59e9
Krausso <apvbd17@gmail.com>
2021-01-28 22:13:08 +0200

    dev; starting refactor

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { HomePayments } from "../../components/HomePayments/HomePayments";
+import { HomeContent } from "../../components/HomeContent/HomeContent";
 import "./Home.scss";
 
 export const Home = () => {
   return (
     <section className="home">
+      <HomeContent />
       <HomePayments />
     </section>
   )
