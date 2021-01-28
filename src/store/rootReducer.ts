aecf36d535741e05dd4077716d56af036cc43c52
Krausso <apvbd17@gmail.com>
2021-01-28 22:11:40 +0200

    dev; initialState updated

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,9 +2,46 @@ import { createReducer } from "@reduxjs/toolkit";
 import { initialStateT } from "../types/initialState.type";
 import { addItemAction } from "./actions/addItem";
 import { addCashAction } from "./actions/addCash";
+import { nanoid } from "nanoid";
 
 const initialState: initialStateT = {
-  items: [],
+  items: [
+    {
+      id: nanoid().toString(),
+      title: "Cookie",
+      price: 0.1,
+      quantity: 3,
+      image_url: "https://art.pixilart.com/67a5851268e95c0.png"
+    },
+    {
+      id: nanoid().toString(),
+      title: "Cookie",
+      price: 0.1,
+      quantity: 3,
+      image_url: "https://art.pixilart.com/67a5851268e95c0.png"
+    },
+    {
+      id: nanoid().toString(),
+      title: "Cookie",
+      price: 0.1,
+      quantity: 3,
+      image_url: "https://art.pixilart.com/67a5851268e95c0.png"
+    },
+    {
+      id: nanoid().toString(),
+      title: "Cookie",
+      price: 0.1,
+      quantity: 3,
+      image_url: "https://art.pixilart.com/67a5851268e95c0.png"
+    },
+    {
+      id: nanoid().toString(),
+      title: "Cookie",
+      price: 0.1,
+      quantity: 0,
+      image_url: "https://art.pixilart.com/67a5851268e95c0.png"
+    }
+  ],
   cash: 0
 }
 
@@ -14,7 +51,7 @@ const rootReducer = createReducer(initialState, {
   },
   [addCashAction.type]: (state, action) => {
     return {...state, cash: state.cash+action.payload}
-  }
+  },
 })
 
 export default rootReducer;
