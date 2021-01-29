439d16ae5537ce4e05def10d96fd506eec505bce
Krausso <apvbd17@gmail.com>
2021-01-29 19:21:27 +0200

    dev; rootReducer update, literal -> builder cases

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -46,21 +46,19 @@ const initialState: initialStateT = {
   cash: 0
 }
 
-const rootReducer = createReducer(initialState, {
-  [addItemAction.type]: (state, action) => {
-    state.items.push(action.payload);
-    return state;
-  },
-  [addCashAction.type]: (state, action) => {
-    state.cash += action.payload;
-    return state;
-  },
-  [buyItemAction.type]: (state, action) => {
-    const itemIndex = state.items.findIndex(i => i.id === action.payload);
-    state.items[itemIndex].quantity -= 1;
-    state.cash -= state.items[itemIndex].price;
-    return state;
+const rootReducer = createReducer(initialState, builder => {
+    builder.addCase(addItemAction, (state, action) => {
+      state.items.push(action.payload);
+    })
+    builder.addCase(addCashAction, (state, action) => {
+      state.cash += action.payload;
+    })
+    builder.addCase(buyItemAction, (state, action) => {
+      const itemIndex = state.items.findIndex(i => i.id === action.payload);
+      state.items[itemIndex].quantity -= 1;
+      state.cash -= state.items[itemIndex].price;
+    })
   }
-})
+)
 
 export default rootReducer;
