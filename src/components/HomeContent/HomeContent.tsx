ec87f4072e6875906e74e4fb30f17b169a13301d
Krausso <apvbd17@gmail.com>
2021-01-30 20:26:20 +0200

    dev; design updated

--- src/components/HomeContent/HomeContent.tsx
+++ src/components/HomeContent/HomeContent.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HomePayments } from "components/HomePayments/HomePayments";
 import { ContentItems } from "./ContentItems/ContentItems";
 import "./HomeContent.scss";
 
@@ -7,6 +8,7 @@ export const HomeContent = () => {
     <section className="home__content">
       <main className="content__machine">
         <h1 className="machine__title">VENDING MACHINE</h1>
+        <HomePayments />
         <div className="machine__items">
           <ContentItems />
         </div>
