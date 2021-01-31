b9b14bcd7d055912ea6750508f0c4aa5ee740b6c
Krausso <apvbd17@gmail.com>
2021-01-31 13:03:04 +0200

    dev; loader added to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { HomeContent } from "components/HomeContent/HomeContent";
 import { CenteredSection } from "../components/CenteredSection/CenteredSection";
+import Loader from "react-loader";
 
 export const Home = () => {
+  const [loadedStatus, setLoadedStatus] = useState(false);
+
+  const loaderOptions = {
+    lines: 12,
+    length: 10,
+    radius: 15,
+    speed: 1.5,
+    color: "#0F9D58",
+    fps: 30,
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadedStatus(true);
+    }, 1500)
+  }, [])
+
   return (
-    <CenteredSection>
-      <HomeContent />
-    </CenteredSection>
+    <Loader loaded={loadedStatus} {...loaderOptions}>
+      <CenteredSection>
+        <HomeContent />
+      </CenteredSection>
+    </Loader>
   )
 };
